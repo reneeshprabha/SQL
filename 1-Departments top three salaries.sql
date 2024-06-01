aee28558b24dfef5da2a62dc76529830f5c13479
reneeshprabha <149580980+reneeshprabha@users.noreply.github.com>
2024-06-01 17:26:55 +0530

    Query to list top 3 salaries in each department
    Query to list employees having top 3 salaries in each department

---
+++ 1-Departments top three salaries.sql
@@ -0,0 +1,46 @@
+Problem: Departments top three salaries
+Write a solution to find the top 3 employees who are high earners in each of the departments.Return the result table in any order.
+
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 85000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
+| 7  | Will  | 70000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Joe      | 85000  |
+| IT         | Randy    | 85000  |
+| IT         | Will     | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+
+
+-------------------------------------------------------------------------
+Query:
+
+SELECT Department , Employee ,Salary FROM (
+    select 
+        d.name as Department,e.name as Employee, e.Salary ,
+        DENSE_RANK() over( partition by d.id order by e.salary DESC)DID
+    from Employee e join Department d on e.departmentid=d.id )A 
+WHERE DID IN (1,2,3)
+-------------------------------------------------------------------------
+
